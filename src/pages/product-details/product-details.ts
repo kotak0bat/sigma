dffe2fb58c4aebc59a2a937056f8c938f11c2604
kotak0bat88 <kotak0bat88@yahoo.com>
2018-05-18 08:36:59 +0700

    add cart

--- src/pages/product-details/product-details.ts
+++ src/pages/product-details/product-details.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ToastController } from 'ionic-angular';
 import * as WC from 'woocommerce-api';
+
+import { Storage } from '@ionic/storage';
 
 
 @Component({
@@ -13,7 +15,7 @@ export class ProductDetailsPage {
   WooCommerce: any;
   reviews: any[] = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage, public toastCtrl: ToastController) {
 
       this.product = this.navParams.get("product");
       console.log(this.product);
@@ -32,6 +34,50 @@ export class ProductDetailsPage {
       }, (err)=> {
         console.log(err);
       })
+  }
+
+  addToCart(product) {
+    this.storage.get("cart").then((data) => {
+      
+      if(data == null || data.length == 0) {
+
+          data = [];
+          data.push({
+            "product": product,
+            "qty": 1,
+            "amount": parseFloat(product.price)
+          });
+
+      } else {
+        let added = 0;
+        for(let i = 0; i < data.length; i++){
+          if(product.id == data[i].product.id){
+            console.log("product is already in the cart");
+            let qty = data[i].qty;
+            data[i].qty = qty+1;
+            data[i].amount = parseFloat(data[i].amount) + parseFloat(data[i].product.price);
+            added = 1;
+          }
+        }
+        if(added == 0)
+        data.push({
+          "product": product,
+          "qty": 1,
+          "amount": parseFloat(product.price)
+        });
+      }
+      
+      this.storage.set("cart", data).then( () => {
+        console.log("Cart Updated");
+        console.log (data);
+
+        this.toastCtrl.create({
+          message: "Cart Updated",
+          duration: 3000
+        }).present();
+      })
+
+    });
   }
 
 
