47fcfc15dbd8c94b617094d6b57001046a9348f4
kotak0bat88 <kotak0bat88@yahoo.com>
2018-05-18 10:55:41 +0700

    Delete Shoping Cart

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ViewController } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 
 
@@ -11,20 +11,49 @@ export class CartPage {
 
   cartItems: any[] = [];
   total: any;
+  showEmptyCartMessage: boolean = false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage, public viewCtrl: ViewController) {
 
+    this.total = 0.0;
+    
     this.storage.ready().then(()=>{
       this.storage.get("cart").then((data)=>{
         this.cartItems = data;
-        console.log(data);
+        console.log(this.cartItems);
+
+        if(this.cartItems.length > 0){
+          this.cartItems.forEach((item, index)=>{
+            this.total = this.total + (item.product.price * item.qty)
+          })
+        } else {
+          this.showEmptyCartMessage = true;
+        }
       })
     })
 
   }
 
+  removeFromCart(item, i){
+    let price = item.product.price;
+    let qty = item.qty;
+
+    this.cartItems.splice(i, 1);
+    this.storage.set("cart", this.cartItems).then (()=>{
+      this.total = this.total - (price * qty);
+    });
+
+    if(this.cartItems.length == 0){
+      this.showEmptyCartMessage = true;
+    }
+  }
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad CartPage');
+  }
+
+  closeModal(){
+    this.viewCtrl.dismiss();
   }
 
 }
